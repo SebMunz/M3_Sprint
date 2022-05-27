20b3484be902d72d594a5f90c7d6c0ae1a7f21c0
Pame-b182 <105446207+Pame-b182@users.noreply.github.com>
2022-05-27 18:51:14 -0400

    Update script telovendosprint.sql

--- script telovendosprint.sql
+++ script telovendosprint.sql
@@ -1,20 +1,25 @@
-create schema TeLoVendo_s3;
 
-use Telovendo_s3;
+create schema TeLoVendo_s3; -- Se crea la base de datos "TeLoVendo_s3".
+
+/* CREATE USER 'Admin_TLV'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON telovendo_s3 TO 'Admin_TLV'@'localhost'
+FLUSH PRIVILEGES; */ -- Se crea el usuario "admin_TLV" con todos los privilegios sobre la base "TeLoVendo_s3" a través de la terminal "My SQl Command Line Client".
+
+use Telovendo_s3; -- Se deja estipulado que se usará esta base de datos en específico.
 
 create table proveedor (
 RUT int unsigned not null primary key,
 nombre_corporativo varchar(50) not null,
 representante_legal varchar(50) not null,
 categoria_producto varchar(50) not null,
-email varchar(40) not null);
+email varchar(40) not null); -- Se crea la tabla "proveedor" con los atributos solicitados.
 
 create table contacto_proveedor (
 id_contacto int unsigned auto_increment not null primary key,
 RUT int unsigned not null,
 telefono int unsigned not null,
 recepcionista varchar(50) not null,
-foreign key (RUT) references proveedor(RUT));
+foreign key (RUT) references proveedor(RUT)); -- Se crea la tabla "contacto_proveedor" para almacenar los teléfonos de cada proveedor, asociado a su RUT.
 
 create table producto (
 SKU varchar(12) not null primary key,
@@ -22,39 +27,39 @@ nombre varchar(25) not null,
 precio int unsigned not null,
 categoria varchar(40) not null,
 color varchar(20) not null,
-stock int unsigned not null);
+stock int unsigned not null); -- Se crea la tabla "producto" con los atributos solicitados.
 
-create table proveedor_producto (
+create table producto_proveedor (
 id_prpv int unsigned not null auto_increment primary key,
 RUT int unsigned not null,
 SKU varchar(12) not null,
-foreign key (RUT) references proveedor(RUT), foreign key (SKU) references producto(SKU));
+foreign key (RUT) references proveedor(RUT), foreign key (SKU) references producto(SKU)); -- Se crea la tabla intermedia "producto_proveedor" que relacione los proveedores con los productos.
 
 create table cliente (
 id_cliente int unsigned not null auto_increment primary key,
 nombre varchar(30) not null,
 apellido varchar(30) not null,
-direccion varchar(60) not null);
+direccion varchar(60) not null); -- Se crea la tabla "cliente" con los atributos solicitados.
 
 create table compra_venta (
 id_venta int unsigned not null auto_increment primary key,
 SKU varchar(12) not null,
 id_cliente int unsigned not null,
-foreign key (SKU) references producto(SKU), foreign key (id_cliente) references cliente(id_cliente));
+foreign key (SKU) references producto(SKU), foreign key (id_cliente) references cliente(id_cliente)); -- Se crea la tabla intermedia "compra_venta" que almacene las compras de los clientes y sus respectivos productos.
 
 insert into proveedor (RUT, nombre_corporativo, representante_legal, categoria_producto, email)
 values (16408529, 'Mayer Group', 'Joela Candlin', 'Notebook', 'jcandlin0@hc360.com'),
 (19664628, 'Wunsch Inc', 'Sandro Boughey', 'Videojuegos', 'sboughey1@surveymonkey.com'),
 (17684370, 'Denesik Inc', 'Blondy Lukesch', 'Televisión', 'blukesch2@wikipedia.org'),
 (16106672, 'Franecki, Dach and Bechtelar', 'Clevey Denial', 'Celulares', 'cdenial3@stanford.edu'),
-(19341684, 'Hammes LLC', 'Burk Semiraz', 'Audio', 'bsemiraz4@slate.com');
+(19341684, 'Hammes LLC', 'Burk Semiraz', 'Audio', 'bsemiraz4@slate.com'); -- Se insertan 5 registros a la tabla "proveedor".
 
 insert into cliente (id_cliente, nombre, apellido, direccion)
 values (1, 'Cristabel', 'Dillimore', '396 Moland Plaza'),
 (2, 'Yves', 'Dyers', '95018 Elmside Drive'),
 (3, 'Nomi', 'Leitche', '37 Hallows Street'),
 (4, 'Noe', 'Pendred', '946 Alpine Alley'),
-(5, 'Dolley', 'Fust', '90559 Mayer Road');
+(5, 'Dolley', 'Fust', '90559 Mayer Road'); -- Se insertan 5 registros a la tabla "cliente".
 
 insert into producto (SKU, nombre, precio, categoria, color, stock)
 values ('A8751B119863', 'Celular Samsung', '94990', 'Celulares', 'Violeta', 7),
@@ -66,10 +71,8 @@ values ('A8751B119863', 'Celular Samsung', '94990', 'Celulares', 'Violeta', 7),
 ('A1544B734556', 'Audífonos', '05990', 'Audio', 'Rojo', 67),
 ('A8493B237377', 'Celular Moto', '71990', 'Celulares', 'Rosado', 58),
 ('A3881B740193', 'Smarttv 10', '99990', 'Television', 'Fucsia', 46),
-('A9058B978748', 'Celular Xiaomi', '90990', 'Celulares', 'Verde', 87);
+('A9058B978748', 'Celular Xiaomi', '90990', 'Celulares', 'Verde', 87); -- Se insertan 10 registros a la tabla "producto".
 
-
-/*Ingreso datos a tabla contacto_proveedor*/
 insert into contacto_proveedor(RUT, telefono, recepcionista)
 values
 ('16106672','951366598','José Muñoz'),
@@ -81,10 +84,9 @@ values
 ('16408529','987878798', 'Jessica Fuentes'),
 ('17684370','996969699', 'Marina  Castro'),
 ('19341684','952545664','Pablo Diaz'),
-('19664628','932659875','Miguel Norambuena');
+('19664628','932659875','Miguel Norambuena'); -- Se insertan 10 registros a la tabla "contacto_proveedor". 
 
-/*Insertamos la relacion producto proveedor*/
-insert into proveedor_producto (SKU, RUT)
+insert into producto_proveedor (SKU, RUT)
 values
 ('A0201B802979','19664628'),
 ('A1544B734556','19341684'),
@@ -93,47 +95,45 @@ values
 ('A5576B513911','19664628'),
 ('A6739B414865','16408529'),
 ('A8060B442883','16408529'),
-('A8493B237377','16106672');
+('A8493B237377','16106672'); -- Se insertan 8 registros con la relación producto (a través de su SKU) y proovedor (a través de su RUT).
 
-/*seleccionamos la categoría, contamos todo y lo traemos como TOP, se ordenan por cantidad y se agrupan por categoría. Le damos un orden descendiente y limitamos a un resultado para saber el más repetido*/
 SELECT categoria, COUNT(*) AS TOP
 FROM producto
 GROUP BY categoria
 ORDER BY TOP DESC
-LIMIT 1;
+LIMIT 1; /* La categoría de productos que más se repite es la de "celulares". Esta se repite 3 veces. */
 
-/* Traemos el nombre y stock de los productos, agrupamos por nombre y ordenamos por stock de mayor a menor */
 select nombre, stock
 from producto
 where stock >= (select avg(stock) from producto)
 group by nombre
-order by stock desc;
+order by stock desc; /* Los 6 productos que tienen un mayor stock o por sobre el promedio son: Celular Xiaomi (87u), Parlante(79u),
+Mario Car (79u), Disco Duro (72u), Audífonos (67u) y Celular Moto (58u). */
 
-/* Seleccionamos color y contamos todo, nombramos el resultado del conteo como TOP. Se agrupan por color y se ordenan por el conteo realizado, limitando al mayor resultado */
 SELECT color, COUNT(*) AS TOP
 FROM producto
 GROUP BY color
 ORDER BY TOP DESC
-LIMIT 1;
+LIMIT 1; /* El color de producto que es más común en la tienda es el rojo. */
 
-/* Proveedores con menor stock */
 select  p1.nombre_corporativo, sum(p.stock) as stock from producto as p
 	inner join proveedor_producto pp on (p.SKU = pp.SKU)
 	inner join proveedor p1 on (p1.RUT = pp.RUT)
 group by p1.nombre_corporativo
 order by stock asc
-limit 2;
+limit 2; /* los 2 proveedores con menor stock de productos son: Denesik Inc (46u) y Franecki y Dach and Bechtelar (58u).*/
 
-/* */
 select categoria, count(categoria) as cantidad from producto
 group by categoria
 order by cantidad desc
-limit 1;
+limit 1; /* Se determina que la categoría de productos más popular es la de "Celulares". */
 
 update producto
 set categoria = 'Electrónica y Computación'
-where SKU in ('A8493B237377','A8751B119863','A9058B978748');
+where SKU in ('A8493B237377','A8751B119863','A9058B978748'); /* Se cambia el nombre de la categoría más popular (Celulares) 
+por "Electrónica y Computación" en la tabla "producto". */
 
 update proveedor 
 set categoria_producto = 'Electrónica y Computación'
-where RUT = 16106672;
+where RUT = 16106672; /* Se cambia el nombre de la categoría más popular (Celulares) 
+por "Electrónica y Computación" en la tabla "proveedor". */
